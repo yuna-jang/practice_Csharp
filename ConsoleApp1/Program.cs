486061e54e46db252d63ebece8e5197349d1069a
YUNA <jya3161@outlook.kr>
2019-12-01 14:43:55 +0900

    20191201

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee pooh = new Employee();
+            pooh.SetName("Pooh");
+            pooh.SetPosition("Waiter");
+            Console.WriteLine($"{pooh.GetName()}, {pooh.GetPosition()}");
+            Console.ReadLine();
+        }       
+    }
+    class Employee
+    {
+        private string Name;
+        private string Position;
+
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+        public string GetName()
+        {
+            return Name;
+        }
+        public void SetPosition(string Position)
+        {
+            this.Position = Position;
+        }
+        public string GetPosition()
+        {
+            return this.Position;
+        }
+    }
+}
